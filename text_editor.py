e32ec2475c73bbee338b7ae336c4c20ad9174e2e
PoeyFonduez <np.chutchomchuen@gmail.com>
2019-06-05 18:16:19 +0700

    text editor fix

--- text_editor.py
+++ text_editor.py
@@ -1,11 +1,21 @@
 #text editor
 #r = read,w = write,a = append if file not exist will create new one,+ = r/w for update
-test_text = open("text_editor.txt","a+r") #open/create file in append+read mode
-test_text.write("This is test text"+'\n')
+#w : will overwrite old one , a : will update the file
 
-#test_text = open("text_editor.txt","r")
+test_text = open("text_editor.txt","w+") #open/create file in write+read mode
+test_text.write("This is test text"+'\n') #str/list is available to write to file
+
+test_text = open("text_editor.txt","r")
 #content = test_text.read() #file reading
 content = test_text.readlines() #file reading line by line
 print(content,type(content))
 
 test_text.close()
+
+# #to change someline of file
+# test_text = open("text_editor.txt","r")
+# content = test_text.readlines()
+# content[2] = "new text to change" #change data in line 2
+# test_text = open("text_editor.txt","w")
+# text_text.write(content) # write whole new file
+
