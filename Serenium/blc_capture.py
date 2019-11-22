867c96f5a6749b5eea2879e01e7289379290e207
Anik <nick.thatsanapremsin@gmail.com>
2019-11-22 18:31:45 +0700

    edit privilege

--- Serenium/blc_capture.py
+++ Serenium/blc_capture.py
@@ -5,6 +5,8 @@ from time import sleep
 import sys
 import datetime, time, schedule
 
+
+
 chrome_option = Options()
 chrome_option.add_experimental_option("detach", True)
 driver = webdriver.Chrome()
@@ -21,33 +23,35 @@ def setUp():
     driver.maximize_window()
     driver.get(base_url)
     driver.implicitly_wait(30)
-    #driver.set_window_size(desktop['1366'], desktop['764'])
 
 def Privilege():
     driver.get("https://pttblc-uat.dockeruat.23perspective.com/th/redeem/privilege")
     driver.implicitly_wait(30)
-    driver.save_screenshot("privilege.png")
+    total_width = driver.execute_script("return document.body.offsetWidth")
+    total_height = driver.execute_script("return document.body.scrollHeight")
+    driver.set_window_size(total_width, total_height)
+    dateSTR = datetime.datetime.now().strftime("%H:%M:%S" )
+    filename = "privilege " + dateSTR + ".png"
+    driver.save_screenshot(filename)
     cur_url = driver.current_url
     print(cur_url)
+    print("Resulution ",total_height," X ",total_width)
 
 def dothing():
-    dateSTR = datetime.datetime.now().strftime("%H:%M:%S" )
-    print(dateSTR)
+    print("- - Program is running - -")
     setUp()
     Privilege()
     driver.close()
-    sys.exit()
+    print("- - END - -")
+    sys.exit()#break
    
+dothing()
 
-
-
-schedule.every().day.at("15:19").do(dothing)
+'''schedule.every().day.at("15:57").do(dothing)
 while True: 
-  
     # Checks whether a scheduled task  
     # is pending to run or not 
     schedule.run_pending() 
     currentTime = datetime.datetime.now().strftime("%H:%M:%S" )
-
     print(currentTime)
-    time.sleep(1) 
+    time.sleep(1) '''
